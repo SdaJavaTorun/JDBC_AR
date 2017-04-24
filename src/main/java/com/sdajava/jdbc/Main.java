e18f10716abb70e5904d4868b66a21e7bfd81d1d
grovas <arczir@gmail.com>
2017-04-24 18:48:46 +0200

    4 commit

--- src/main/java/com/sdajava/jdbc/Main.java
+++ src/main/java/com/sdajava/jdbc/Main.java
@@ -1,8 +1,49 @@
 package com.sdajava.jdbc;
 
+import java.sql.*;
+
 public class Main {
 
+    public static final String JDBC_DRIVER="com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL
+            = "jdbc:mysql://localhost/ksiegarnia?" +
+            "useSSL=false&useJDBCCompliantTimezoneShift=" +
+            "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public final static String USER="root";
+    public final static String PASS="";
+
     public static void main(String[] args) {
-	// write your code here
+
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Lacze z BD...");
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+            statement = connection.createStatement();
+            ResultSet resultSet =
+                    statement.executeQuery("select * from books");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("title") +
+                 " " + resultSet.getString("author") +
+                " " + resultSet.getInt("page_count") +
+                " " + resultSet.getDouble("price") + " zł" +
+                " " + resultSet.getString("category") +
+                " " + resultSet.getString("isbn"));
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
     }
 }
